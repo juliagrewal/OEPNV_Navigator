a6186322a0cbbd05eb98b1b673c30db530452edd
wada1028 <wada1028@hs-karlsruhe.de>
2020-01-14 19:09:41 +0100

    Map in "Karte" not working ohne absturz

--- app/src/main/java/wada1028/info3/oepnv_navigator/ui/tools/ToolsFragment.java
+++ app/src/main/java/wada1028/info3/oepnv_navigator/ui/tools/ToolsFragment.java
@@ -5,11 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapFragment;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 import wada1028.info3.oepnv_navigator.R;
 
 public class ToolsFragment extends Fragment {
     MapView mapView;
+    MapFragment mapFragment;
+    LatLng karlsruhe;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -19,11 +32,24 @@ public class ToolsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        MapboxAccountManager.start(getContext(),getString(R.string.access_token));
+        mapView = (MapView) view.findViewById(R.id.mapViewTools);
+        karlsruhe = new LatLng(49,8);
+        MapboxMapOptions options = new MapboxMapOptions();
+        options.styleUrl(Style.LIGHT);
+        options.camera(new CameraPosition.Builder()
+                .target(karlsruhe)
+                .zoom(10)
+                .build());
+        mapFragment = MapFragment.newInstance(options);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
 
-        if(view!=null) {
-            mapView = (MapView) view.findViewById(R.id.mapViewTools);
-            mapView.onCreate(savedInstanceState);
-        }
+            }
+        });
+
+        mapView.onCreate(savedInstanceState);
     }
 
     @Override
